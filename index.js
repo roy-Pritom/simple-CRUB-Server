f03d283fb3707a88361354e42230f6c19e39be92
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-05-08 22:51:49 +0530

    add my code

---
+++ index.js
@@ -0,0 +1,80 @@
+const express=require('express');
+const app=express();
+const port=process.env.PORT || 4000;
+const {   ObjectId } = require('mongodb');
+
+
+const cors=require('cors')
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// roypritom52100z60
+// fil82il2HLIgzTXP
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://roypritom52100z60:fil82il2HLIgzTXP@cluster0.ow3zltf.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+     
+      
+      const userCollection =  client.db("userDB").collection("users");
+ 
+      app.get('/user',async(req,res)=>{
+        const cursor=userCollection.find();
+        const result=await cursor.toArray();
+        res.send(result);
+      })
+  
+      app.post('/user',async(req,res)=>{
+          const user=req.body;
+          console.log(user);
+       
+          const result = await userCollection.insertOne(user);
+  
+          res.send(result)
+      });
+
+      app.delete('/user/:id',async(req,res)=>{
+        const id=req.params.id;
+        // console.log(id);
+        const query={_id:new ObjectId(id)}
+        const result=await userCollection.deleteOne(query)
+        res.send(result)
+
+      })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send('MOnogodb')
+})
+
+app.listen(port,()=>{
+    console.log(`Server running port ${port}`);
+})
